60bbb9398712d72faced9c71d07d97c9ce897822
Tuurmeeus <50370038+Tuurmeeus@users.noreply.github.com>
2021-10-05 16:21:03 +0200

    Dynamische Hello World

---
+++ Session1/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    println("Hello world");
+
+    val namen = arrayOf("Yassin", "Cosima", "Rob", "Amine", "Rokus")
+
+    for(student in namen){
+        println("Goeiedag"+ " " + student)
+    }
+}
