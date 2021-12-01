6b719cf1293447c0bfcca9ee53ce2e821001bf9d
Tuurmeeus <50370038+Tuurmeeus@users.noreply.github.com>
2021-12-01 16:16:00 +0100

    Challenge-2 completed

--- Session2/minigame/src/main/kotlin/Main.kt
+++ Session2/minigame/src/main/kotlin/Main.kt
@@ -16,10 +16,38 @@ fun challengeOne(){
 
     if (correctAnswer.contains(answer)){
         println("Well done traveler, you advance to the next level.")
+        challengeTwo()
     } else{
         gameOver()
     }
 
+
+}
+
+fun challengeTwo(){
+    println()
+    println()
+    println("Guess the dice roll: low or high?")
+    var guess : String
+
+    do {
+        guess = readLine().toString()
+    } while (!setOf<String>("low", "high").contains(guess))
+    val diceRoll = rollDice(6)
+    println("The dice rolled: $diceRoll")
+    val possibleResults = if(guess == "low") setOf<Int>(1, 2, 3) else setOf<Int>(4, 5, 6)
+    if(possibleResults.contains(diceRoll)){
+        println("Good job 1 step closer to survival")
+    } else{
+        gameOver()
+    }
+
+
+
+}
+
+fun rollDice(sides: Int): Int{
+    return(Math.random() * sides + 1).toInt()
 
 }
 
