ab286b5899f37ff288d9c3a27e070e33800bf60c
Tuurmeeus <50370038+Tuurmeeus@users.noreply.github.com>
2021-12-02 13:44:13 +0100

    Chatroulette task3 completed

---
+++ Session3/Chatroulette/src/main/kotlin/ChatBot.kt
@@ -0,0 +1,8 @@
+class ChatBot: ChatPartner {
+    override fun introduce(){
+        println("Hello I'm clippy!")
+    }
+    override fun talk(){
+        println("Did you know that I'm a chatbot?")
+    }
+}
